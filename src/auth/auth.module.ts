4f564e22408865e5aeccbeb371a81e79e476f9a9
Dreezy305 <bankoleidris@gmail.com>
2023-03-09 17:48:20 +0100

    auth creation, user dto

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [AuthService, JwtStrategy, PrismaService],
+  controllers: [AuthController],
+  imports: [JwtModule.register({})],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,4 +10,46 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async signUp(dto: CreateUserDto) {
+    await this.prisma.$connect();
+    const hash = await argon.hash(dto.password);
+    const checkIfUserExist = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (checkIfUserExist) {
+      throw new ForbiddenException('User with these credentials already exist');
+    }
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name: dto.name,
+          email: dto.email,
+        },
+      });
+      return {
+        data: user,
+        message: 'Account created successfully',
+        success: true,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException(
+          'User with these credentials already exist',
+        );
+      } else if (error instanceof PrismaClientValidationError) {
+        throw new ForbiddenException('Invalid credentials');
+      } else {
+        throw error;
+      }
+    }
+  }
+}
