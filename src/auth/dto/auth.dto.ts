2e126e4f3b71d216a2113d59cc29771e8cb509f2
Dreezy305 <bankoleidris@gmail.com>
2023-03-09 16:57:16 +0100

    validation pipes, auth dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,61 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsAlpha,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
-    description: 'user mail',
+    description: 'user email',
     type: String,
     required: true,
   })
+  @IsEmail()
+  @IsNotEmpty()
+  @Matches(
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    { message: 'email must be a valid email' },
+  )
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'password must contain Min of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character',
+    },
+  )
+  @ApiProperty({
+    description: 'user password',
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'user first name',
+    type: String,
+    required: true,
+  })
+  @IsAlpha()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'user phone number',
+    type: String,
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
 }
