612bd5833ffa671e303e556ff93ff2e77cd46f2f
Dreezy305 <bankoleidris@gmail.com>
2023-03-09 15:59:40 +0100

    prisma, validation, documentation etc

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const config: any = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('User management system')
+    .setDescription(
+      'This is the official User management API document for skywallet assessment',
+    )
+    .setVersion('1.0.0')
+    .addServer('')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('v1/api');
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
